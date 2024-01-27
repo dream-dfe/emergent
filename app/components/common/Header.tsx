8b1dc2b40a2d2506b5146beed67b8c7c6375c641
de-mawo <groovebassist1@gmail.com>
2024-01-27 17:38:28 +0200

    Git merge error's fix

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -7,10 +7,14 @@ import SideDrawer from "./SideDrawer";
 
 
 const Header = () => {
+ 
   return (
     <header className="sticky top-0 z-40 bg-white grid grid-cols-2  lg:grid-cols-3 p-8 items-center">
-      <div>
-        <Image src="/emergent-logob.png" width={50} height={50} alt="logo" />
+      {/* Left Area  */}
+      <div className="flex items-center gap-x-8  ">
+        <Link href={`/`} className="p-2 ">
+          <Image src={`/emergent-logob.png`} width={50} height={50} alt="logo" />
+        </Link>
       </div>
       <div className="hidden lg:flex  items-center space-x-20">
         <Link href="/" className="font-semibold text-lg text-slate-600">
