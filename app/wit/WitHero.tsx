9e2349dda5ec461ba8df4ae4ec71c0178b72df5b
de-mawo <groovebassist1@gmail.com>
2024-08-19 12:49:50 +0200

    Fixed Banner Text on WiT

--- app/wit/WitHero.tsx
+++ app/wit/WitHero.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function WitHero() {
   return (
-    <section className="relative flex h-[60vh] w-full flex-col items-start justify-start md:items-center md:justify-center">
+    <section className="relative flex h-[80vh] md:h-[60vh] w-full flex-col">
       <div className="absolute -z-10 h-full w-full overflow-hidden">
         <Image
           src="/img/wit-banner.jpg"
@@ -14,19 +14,32 @@ export default function WitHero() {
           alt="hero-img"
         />
       </div>
-      <div className="absolute bottom-8 left-12">
-        {" "}
-        <Button
-          asChild
-          className="min-w-48 rounded-full bg-[#38D200] hover:bg-[#0473ea]"
-        >
-          <Link
-            href="https://docs.google.com/forms/d/e/1FAIpQLSd49LC589BOM-PR7LmkpiuGTN1ellYd-vbbNbM0OQJSwDStUw/viewform"
-            className="text-4xl"
+      <div className="absolute md:top-10 ml-5 grid gap-10">
+        <h1 className="text-2xl leading-tight md:text-6xl">
+          <span className="text-white">Standard Chartered </span> <br />
+          <span className="text-[#38D200]">Women in Tech </span> <br />
+          <span className="text-white"> Accelerator Programme</span>
+        </h1>
+        <p className="text-xl md:text-4xl">
+          Get Expert Training, Coaching & <br /> Secure Seed Funding for your
+          Business!
+        </p>
+      </div>
+      <div className="absolute bottom-8 w-full">
+        <div className="mx-5 flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
+          <Button
+            asChild
+            className="min-w-56 rounded-full bg-[#38D200] hover:bg-[#0473ea]"
           >
-            APPLY NOW{" "}
-          </Link>
-        </Button>{" "}
+            <Link href="https://docs.google.com/forms/d/e/1FAIpQLSd49LC589BOM-PR7LmkpiuGTN1ellYd-vbbNbM0OQJSwDStUw/viewform">
+              <span className="text-lg md:text-2xl"> APPLY NOW</span>{" "}
+            </Link>
+          </Button>{" "}
+          <h2 className="text-xl text-white md:text-2xl lg:text-4xl">
+            {" "}
+            Deadline for application is 07<sup>th</sup> September 2024
+          </h2>
+        </div>
       </div>
     </section>
   );
