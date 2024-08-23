d7b1fbc4ba4dce63afb72e10d6ecd139a1e36641
de-mawo <groovebassist1@gmail.com>
2024-08-23 16:51:15 +0200

    Added Permisions

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Fira_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { auth } from "@clerk/nextjs/server";
+import { SyncActiveOrganization } from "@/components/common/SyncActiveOrganization";
 
 const fira = Fira_Sans({
   subsets: ["latin"],
@@ -20,8 +22,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const { userId, sessionClaims } = auth();
+
   return (
     <ClerkProvider afterSignOutUrl="/">
+      <SyncActiveOrganization membership={sessionClaims?.membership} />
       <html lang="en">
         <body className={`${fira.variable} `}>{children}</body>
       </html>
